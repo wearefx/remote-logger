36bb33f2ce703e1cb56ca9ee122527524031afda
Warren Moore <warren@warrenmoore.co.uk>
2020-02-26 14:36:44 +0000

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+
+const app = express();
+const certDirPath = "./.dev";
+const port = 8082;
+
+app.use(cors());
+app.use(bodyParser({ limit: "50mb" }));
+app.use(express.json());
+
+app.post("/remote-logger", (req, res) => {
+  fs.writeFileSync("./logs.txt", req.body.map(JSON.stringify).join("\n"), {
+    flag: "a"
+  });
+
+  res.sendStatus(201);
+});
+
+if (certDirPath) {
+  const tlsCertPath = `${certDirPath}/dev.crt`;
+  const tlsKeyPath = `${certDirPath}/dev.key`;
+
+  try {
+    const files = fs.readdirSync(certDirPath);
+
+    const ca = files.filter(file => {
+      return file.startsWith("i") && file.endsWith(".crt");
+    });
+
+    const tls = {
+      key: fs.readFileSync(tlsKeyPath, "utf8"),
+      cert: fs.readFileSync(tlsCertPath, "utf8")
+    };
+
+    if (ca) {
+      tls.ca = ca.map(c => {
+        return fs.readFileSync(`${certDirPath}/${c}`, "utf8");
+      });
+    }
+
+    const httpsServer = https.createServer(tls, app);
+    httpsServer.listen(port);
+    console.log(`Listening (https) on port ${port}`);
+    return;
+  } catch (e) {
+    console.error("Could not start server in https mode: ", e);
+  }
+}
+
+const httpServer = http.createServer(app);
+httpServer.listen(port);
+console.log(`Listening (http) on port ${port}`);
